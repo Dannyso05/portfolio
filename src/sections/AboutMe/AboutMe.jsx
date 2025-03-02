dfa1dd1ae3e73337bb6c105316a8a86e0e5fcd0d
Song Danny <Daniel Song>
2025-03-01 21:21:01 -0500

    edits in aboutme

--- src/sections/AboutMe/AboutMe.jsx
+++ src/sections/AboutMe/AboutMe.jsx
@@ -6,15 +6,17 @@ function AboutMe() {
     <section id="AboutMe" className={styles.aboutMeSection}>
       <div className={styles.titlecontainer}>
       <h1 className={styles.largertitle}>About me</h1>
-      <h3 className={styles.smallertitle}>About me</h3>
+      <h3 className={styles.smallertitle}>About me.</h3>
       </div>
       <div className={styles.contentContainer}>
-        <img src={abt} alt="Profile" className={styles.profilePicture} />
+           <div className={styles.profilePictureContainer}>
+            <img src={abt} alt="Profile" className={styles.profilePicture} />
+          </div>        
         <div className={styles.textContent}>
           <p className={styles.aboutText}>
             Hello, I am Daniel Song. I am a second year student at University of Waterloo, I have high interest in latest Machine Learning and AI technologies. I'm looking to connect with professionals for insights and potential opportunities in this field.
           </p>
-          <a className={styles.educationButton} href="https://uwaterloo.ca/">
+          {/* <a className={styles.educationButton} href="https://uwaterloo.ca/">
             <img 
             src="https://upload.wikimedia.org/wikipedia/en/6/6e/University_of_Waterloo_seal.svg" alt="UWaterloo" className={styles.schoolLogo} />
             <div className={styles.buttonText}>
@@ -22,7 +24,7 @@ function AboutMe() {
               <p>Bachelor of Statistics & Computational Mathematics</p>
               <p>2023 - 2028</p>
             </div>
-          </a>
+          </a> */}
         </div>
       </div>
     </section>
